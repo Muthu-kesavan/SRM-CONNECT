956f8f6826f2f2efd944851b3debd594da6d94ed
Muthu kesavan S <muthukesavan6044@gmail.com>
2024-01-03 22:05:32 +0530

    Authentiaction implement

--- client/src/page/Register/Register.jsx
+++ client/src/page/Register/Register.jsx
@@ -3,60 +3,97 @@ import {useNavigate} from "react-router-dom";
 import { useDispatch } from "react-redux";
 import axios from "axios";
 import { loginFailed, loginStart, loginSuccess } from '../../redux/userSlice';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 const Register = () => {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [register_no, setRegister_no] = useState("");
-  const [password, setPassword] = useState("");
-  const [campus, setCampus] = useState("");
+  const [formData, setFormData] = useState({
+    username: '',
+    email: '',
+    register_no: '',
+    campus: '',
+    password: '',
+  });
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
 
   const handleSignup = async(e)=>{
     e.preventDefault();
     dispatch(loginStart());
+
+    const { username, email, register_no, campus, password } = formData;
+
+    if (
+      !username ||username.length < 3 ||
+      !email ||
+      !/^[a-zA-Z]{2}\d{4}@srmist\.edu\.in$/.test(email) ||
+      !register_no ||
+      !/^[a-zA-Z]{2}\d{13}$/.test(register_no) ||
+      !campus ||
+      !password
+    ) {
+      return toast.error('Please enter valid details.');
+    }
+
     try{
       const res = await axios.post("/auth/signup",{username, email, register_no,campus,password});
       dispatch(loginSuccess(res.data));
-      navigate("/");
-    } catch(err){
+      toast.success("Account created Successfully");
+      setTimeout(()=>{
+        navigate("/");
+      },1000);
+    }catch(err){
       dispatch(loginFailed());
+      toast.error('Registration failed. Please try again.');
     }
 
   };
 
   return (
-    <form className="bg-gray-50 flex flex-col py-12 px-8 rounded-lg w-8/12 md:w-6/12 mx-auto gap-10">
+    <div>
+    <ToastContainer />
+    <form className="bg-gray-100 flex flex-col py-12 px-8 rounded-lg w-8/12 md:w-6/12 mx-auto gap-10">
      <h2 className="text-3xl font-bold text-center text-[#5FBDFF] ">Sign Up</h2>
 
      <input
-     onChange={(e)=> setUsername(e.target.value)}
+     onChange={handleInputChange}
+     value={formData.username}
+     name="username"
      type="text"
      placeholder="Name"
      required
      className="bg-transparent text w-full text-base p-4 rounded-[0.4rem] border-[0.1rem] border-solid focus:border-[0.1rem] focus:border-solid focus:border-[#2196f3]"
      />
      <input
-     onChange={(e)=> setEmail(e.target.value)}
+     onChange={handleInputChange}
+     value={formData.email}
+     name="email"
      type="email"
-     placeholder="Offical Email"
+     placeholder="Official Email"
      required
      className="bg-transparent text w-full text-base p-4 rounded-[0.4rem] border-[0.1rem] border-solid focus:border-[0.1rem] focus:border-solid focus:border-[#2196f3]"
      />
      <input
-     onChange={(e)=> setRegister_no(e.target.value)}
+     onChange={handleInputChange}
+     value={formData.register_no}
+     name="register_no"
      type="text"
      placeholder="Register No"
      required
      className="bg-transparent text w-full text-base p-4 rounded-[0.4rem] border-[0.1rem] border-solid focus:border-[0.1rem] focus:border-solid focus:border-[#2196f3]"
      />
      <select
-        value={campus} 
-        onChange={(e) => setCampus(e.target.value)}
+        value={formData.campus}
+        onChange={handleInputChange}
+        name="campus"
         required
         className='bg-transparent text w-full text-base p-4 rounded-[0.4rem] border-[0.1rem] border-solid focus:border-[0.1rem] focus:border-solid focus:border-[#2196f3]'
       >
-        <option value="">Your Campus*</option> {/* Default option */}
+        <option value="">Your Campus</option> 
         <option value="KTR">KTR</option>
         <option value="RMP">RMP</option>
         <option value="VDP">VDP</option>
@@ -64,7 +101,9 @@ const Register = () => {
       </select>
 
      <input
-     onChange={(e)=> setPassword(e.target.value)}
+     onChange={handleInputChange}
+     value={formData.password}
+     name="password"
      type="password"
      placeholder="Password"
      required
@@ -76,6 +115,7 @@ const Register = () => {
        Sign up
      </button>
     </form>
+    </div>
   )
 }
 
--- client/src/page/Signin/Signin.jsx
+++ client/src/page/Signin/Signin.jsx
@@ -3,7 +3,8 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { loginStart, loginSuccess, loginFailed } from '../../redux/userSlice';
-
+import { ToastContainer, toast} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 const Signin = () => {
   const [register_no, setRegister_no] = useState("");
   const [password, setPassword] = useState("");
@@ -15,14 +16,21 @@ const Signin = () => {
     try{
       const res = await axios.post("auth/signin", { register_no, password });
       dispatch(loginSuccess(res.data));
-      navigate("/");
+      const {username} = res.data;
+      toast.success(`Welcome back ${username}`);
+      setTimeout(() => {
+        navigate("/");
+      }, 1000);
     } catch (err){
       dispatch(loginFailed());
+      toast.error("Incorrect Credentials")
       console.log(err);
     }
   };
   return (
-    <form className="bg-gray-50 flex flex-col py-12 px-8 rounded-lg w-8/12 md:w-6/12 mx-auto gap-10">
+  <div>
+    <ToastContainer />
+    <form className="bg-gray-100 flex flex-col py-12 px-8 rounded-lg w-8/12 md:w-6/12 mx-auto gap-10">
      <h2 className="text-3xl font-bold text-center text-[#5FBDFF] ">LOG IN</h2>
 
      <input
@@ -37,7 +45,7 @@ const Signin = () => {
      placeholder="Password"
      className="bg-transparent text w-full text-base p-4 rounded-[0.4rem] border-[0.1rem] border-solid focus:border-[0.1rem] focus:border-solid focus:border-[#2196f3]"
      />
-     <button className="bg-[#5FBDFF] text-[white] font-[bold] cursor-pointer text-base uppercase px-8 py-4 rounded-[0.4rem] border-[none] hover:bg-[#2196f3]"
+     <button className="bg-[#5FBDFF] text-[white] font-[bold] cursor-pointer text-base uppercase px-8 py-4 rounded-[0.4rem] border-[none] hover:bg-[#2196F3] rounded md"
      onClick={handleLogin}>
        Log in
      </button>
@@ -45,6 +53,7 @@ const Signin = () => {
       <Link to="/signup" className="apply text-[#5FBDFF] uppercase hover:text-[#2196F3]"> Create One</Link>
      </span>
     </form>
+    </div>
   )
 }
 
