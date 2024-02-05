2cca112ff48a3a1539ea3f81bc640160c7cd3679
Muthu kesavan S <muthukesavan6044@gmail.com>
2024-02-05 15:37:26 +0530

    deploy

--- server/index.js
+++ server/index.js
@@ -8,38 +8,35 @@ import authRoutes from './routes/auths.js';
 import postRoutes from './routes/posts.js';
 
 const corsOptions = {
-  origin: [
-    "https://srm-connect.netlify.app",
+    origin:["https://srm-connect.netlify.app,",
     "https://srm-connect.netlify.app/",
     "https://srm-connect.netlify.app/signin/",
     "https://srm-connect.netlify.app/signup/",
     "https://srm-connect.netlify.app/explore/",
     "https://srm-connect.netlify.app/profile/:id"
-  ]
-};
+]
+  }
 
 const app = express();
 dotenv.config();
 
-const connect = () => {
-  mongoose.set("useNewUrlParser", true);  // Add this line to fix a warning
-  mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      console.log("Connected to Database");
-    })
-    .catch((err) => {
-      console.log("Error:", err);
-    });
-};
 
-app.use(cors(corsOptions));
+const connect =()=>{
+    mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.MONGO)
+    .then(()=>{
+        console.log("Connected to Database")
+    }).catch((err)=> {
+        console.log("Error")
+})
+ };
+ app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
-app.use('/users', userRoutes);  // Use relative paths here
-app.use('/auth', authRoutes);
-app.use('/posts', postRoutes);
-
-app.listen(8000, () => {
-  connect();
-  console.log("Listening to the port 8000");
+app.use('https://dataflow-412p.onrender.com/users', userRoutes);
+app.use('https://dataflow-412p.onrender.com/auth', authRoutes);
+app.use('https://dataflow-412p.onrender.com/posts', postRoutes);
+app.listen(8000, ()=>{
+    connect();
+    console.log("Listening to the port 8000");
 });
